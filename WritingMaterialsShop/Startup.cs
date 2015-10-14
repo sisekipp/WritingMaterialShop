5d7804b23f0fa88fdc32e793568d26c1c1d86e66
Sebastian Kipping <kipping.sebastian@gmail.com>
2015-10-14 12:48:35 +0200

    Init Commit

---
+++ WritingMaterialsShop/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WritingMaterialsShop.Startup))]
+namespace WritingMaterialsShop
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
